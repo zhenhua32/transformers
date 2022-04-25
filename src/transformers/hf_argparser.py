846f77e20172e8e3362b52e45da48faf057694c5
zhenhua32 <zhenhua32@gmail.com>
2022-04-25 19:26:16 +0800

    read

--- src/transformers/hf_argparser.py
+++ src/transformers/hf_argparser.py
@@ -29,6 +29,7 @@ DataClassType = NewType("DataClassType", Any)
 
 # From https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
 def string_to_bool(v):
+    """将字符串解析为 bool 类型"""
     if isinstance(v, bool):
         return v
     if v.lower() in ("yes", "true", "t", "y", "1"):
@@ -48,6 +49,7 @@ class HfArgumentParser(ArgumentParser):
     The class is designed to play well with the native argparse. In particular, you can add more (non-dataclass backed)
     arguments to the parser after initialization and you'll get the output back after parsing as an additional
     namespace. Optional: To create sub argument groups use the `_argument_group_name` attribute in the dataclass.
+    要创建 子参数组, 使用 `_argument_group_name` 属性
     """
 
     dataclass_types: Iterable[DataClassType]
@@ -60,14 +62,18 @@ class HfArgumentParser(ArgumentParser):
             kwargs:
                 (Optional) Passed to `argparse.ArgumentParser()` in the regular way.
         """
+        # 添加默认的帮助信息
         # To make the default appear when using --help
         if "formatter_class" not in kwargs:
             kwargs["formatter_class"] = ArgumentDefaultsHelpFormatter
+        # 使用其他参数初始化
         super().__init__(**kwargs)
+        # 如果是单个的, 就变成数组
         if dataclasses.is_dataclass(dataclass_types):
             dataclass_types = [dataclass_types]
         self.dataclass_types = list(dataclass_types)
         for dtype in self.dataclass_types:
+            # 将所有 dataclass 一个个添加进去
             self._add_dataclass_arguments(dtype)
 
     @staticmethod
@@ -76,6 +82,7 @@ class HfArgumentParser(ArgumentParser):
         kwargs = field.metadata.copy()
         # field.metadata is not used at all by Data Classes,
         # it is provided as a third-party extension mechanism.
+        # 居然不应该是 str
         if isinstance(field.type, str):
             raise RuntimeError(
                 "Unresolved type detected, which should have been done with the help of "
@@ -83,6 +90,7 @@ class HfArgumentParser(ArgumentParser):
             )
 
         origin_type = getattr(field.type, "__origin__", field.type)
+        # 没看懂, 跳过
         if origin_type is Union:
             if len(field.type.__args__) != 2 or type(None) not in field.type.__args__:
                 raise ValueError("Only `Union[X, NoneType]` (i.e., `Optional[X]`) is allowed for `Union`")
@@ -99,6 +107,7 @@ class HfArgumentParser(ArgumentParser):
         if isinstance(field.type, type) and issubclass(field.type, Enum):
             kwargs["choices"] = [x.value for x in field.type]
             kwargs["type"] = type(kwargs["choices"][0])
+            # 这个字段有两种情况, 一种是有默认值的, 没有默认值就是必填的
             if field.default is not dataclasses.MISSING:
                 kwargs["default"] = field.default
             else:
@@ -134,8 +143,11 @@ class HfArgumentParser(ArgumentParser):
                 kwargs["default"] = field.default_factory()
             else:
                 kwargs["required"] = True
+        # 略过上面的了, 主要应该就是将 dataclass 的字段转换成 argparse 需要的格式, 也就是那些 kwargs
+        # 添加一个参数
         parser.add_argument(field_name, **kwargs)
 
+        # 这边就是针对 bool 类型的参数, 加一个 no_* 的参数, 用于否定
         # Add a complement `no_*` argument for a boolean field AFTER the initial field has already been added.
         # Order is important for arguments with the same destination!
         # We use a copy of earlier kwargs because the original kwargs have changed a lot before reaching down
@@ -145,11 +157,13 @@ class HfArgumentParser(ArgumentParser):
             parser.add_argument(f"--no_{field.name}", action="store_false", dest=field.name, **bool_kwargs)
 
     def _add_dataclass_arguments(self, dtype: DataClassType):
+        # 这个是用于定义子参数组的, 如果有 _argument_group_name 属性, 也应该是 ArgumentParser 类别的
         if hasattr(dtype, "_argument_group_name"):
             parser = self.add_argument_group(dtype._argument_group_name)
         else:
             parser = self
 
+        # 获取类型提示
         try:
             type_hints: Dict[str, type] = get_type_hints(dtype)
         except NameError:
@@ -159,16 +173,21 @@ class HfArgumentParser(ArgumentParser):
                 f"Evaluation of Annotations (PEP 563)"
             )
 
+        # 遍历所有的字段
         for field in dataclasses.fields(dtype):
+            # 如果没有初始化过, 就跳过
             if not field.init:
                 continue
+            # 添加类别
             field.type = type_hints[field.name]
+            # 解析字段
             self._parse_dataclass_field(parser, field)
 
     def parse_args_into_dataclasses(
         self, args=None, return_remaining_strings=False, look_for_args_file=True, args_filename=None
     ) -> Tuple[DataClass, ...]:
         """
+        这个就是逆向的了, 也能将 argparse 的参数转换成 dataclass 的实例, 跳过不看
         Parse command-line args into instances of the specified dataclass types.
 
         This relies on argparse's `ArgumentParser.parse_known_args`. See the doc at:
@@ -236,6 +255,7 @@ class HfArgumentParser(ArgumentParser):
             inputs = {k: v for k, v in data.items() if k in keys}
             obj = dtype(**inputs)
             outputs.append(obj)
+        # 这是什么奇怪的写法, 也就是说返回一个元组, 这和 tuple(outputs) 有什么区别?
         return (*outputs,)
 
     def parse_dict(self, args: dict) -> Tuple[DataClass, ...]:
@@ -243,6 +263,8 @@ class HfArgumentParser(ArgumentParser):
         Alternative helper method that does not use `argparse` at all, instead uses a dict and populating the dataclass
         types.
         """
+        # 这个本质上和上面的 parse_json_file 是一样的, parse_json_file 就是多了一个读取 json 文件的过程
+        # 如果我来写, 应该不会写两遍
         outputs = []
         for dtype in self.dataclass_types:
             keys = {f.name for f in dataclasses.fields(dtype) if f.init}
