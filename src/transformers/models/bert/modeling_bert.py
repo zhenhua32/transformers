e5c400edeacb250efebc909d8fdd8a7b9b063ba9
zhenhua32 <zhenhua32@gmail.com>
2022-07-02 19:32:15 +0800

    read

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -190,22 +190,31 @@ def load_tf_weights_in_bert(model, config, tf_checkpoint_path):
 
 
 class BertEmbeddings(nn.Module):
-    """Construct the embeddings from word, position and token_type embeddings."""
+    """
+    bert 的嵌入层
+    Construct the embeddings from word, position and token_type embeddings."""
 
     def __init__(self, config):
         super().__init__()
+        # 词嵌入层
         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
+        # 位置嵌入
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        # token 类型嵌入
         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
 
+        # 名称是故意用驼峰式的, 为了能从 tf 的 checkpoints 中导入
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         # position_ids (1, len position emb) is contiguous in memory and exported when serialized
+        # 位置嵌入的类型
         self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
+        # 注册 position_ids, 形状是 (1, max_position_embeddings)
         self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
         if version.parse(torch.__version__) > version.parse("1.6.0"):
+            # 注册 token_type_ids
             self.register_buffer(
                 "token_type_ids",
                 torch.zeros(self.position_ids.size(), dtype=torch.long),
@@ -215,11 +224,13 @@ class BertEmbeddings(nn.Module):
     def forward(
         self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None, past_key_values_length=0
     ):
+        # 输入的形状
         if input_ids is not None:
             input_shape = input_ids.size()
         else:
             input_shape = inputs_embeds.size()[:-1]
 
+        # 序列的长度
         seq_length = input_shape[1]
 
         if position_ids is None:
@@ -229,6 +240,7 @@ class BertEmbeddings(nn.Module):
         # when its auto-generated, registered buffer helps users when tracing the model without passing token_type_ids, solves
         # issue #5664
         if token_type_ids is None:
+            # token_type_ids 可以不传, 直接从 self 中取, 因为已经在构造函数中注册了
             if hasattr(self, "token_type_ids"):
                 buffered_token_type_ids = self.token_type_ids[:, :seq_length]
                 buffered_token_type_ids_expanded = buffered_token_type_ids.expand(input_shape[0], seq_length)
@@ -236,14 +248,18 @@ class BertEmbeddings(nn.Module):
             else:
                 token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
 
+        # 最终用的是 inputs_embeds
         if inputs_embeds is None:
             inputs_embeds = self.word_embeddings(input_ids)
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
+        # 嵌入层是将 inputs_embeds 和 token_type_embeddings 加起来
         embeddings = inputs_embeds + token_type_embeddings
         if self.position_embedding_type == "absolute":
+            # 如果位置嵌入的类型是绝对的, 也加起来
             position_embeddings = self.position_embeddings(position_ids)
             embeddings += position_embeddings
+        # 然后经过层正则化 和 dropout
         embeddings = self.LayerNorm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
